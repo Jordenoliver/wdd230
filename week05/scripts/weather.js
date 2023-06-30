e44755efc2d8149bd2e214587a78d39ae64fab8d
Jordenoliver <oli22011@byui.edu>
2023-06-30 16:28:27 -0700

    edits

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,32 +1,32 @@
-// select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier&units=imperial&appid=391909525f5976c68538054fa4fcec7e';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.7557&lon=6.6394&appid=391909525f5976c68538054fa4fcec7e&units=imperial';
 
-async function apiFetch() {
-  try {
-      const response = await fetch(url);
-      if (response.ok) {
-          const data = await response.json();
-          console.log(data);
-          displayResults(data);
-      } else {
-          throw Error(response.text);
-      }
-  } catch (error) {
-          console.log(error);
-      }
+
+async function apiFetch() { 
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        }else {
+            throw Error(await response.text());
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
 }
+
 apiFetch();
 
 function displayResults(data) {
-  currentTemp.innerHTML = (`${data.main.temp} &deg;F`);
-  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  let desc = data.weather[0].description;
-  weatherIcon.setAttribute('src', iconsrc);
-  weatherIcon.setAttribute('alt', desc);
- 
-  captionDesc.textContent = `${desc}`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
 }
